0bad6649ddbd0ac079885d03734645d411903197
dwijerathna <dwijerathna@netstarter.com>
2016-11-07 17:09:17 +0530

    add todo added

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,4 @@
-
-
-
+// todo JS functionality
 
 var todoList = {
     todos:[],
@@ -9,16 +7,19 @@ var todoList = {
         //alert(this.todos);
         for(var i=0; i < this.todos.length; i++){
             this.todos[i];
+
         }
     },
     addTodo:function(todo){
         this.todos.push(todo);
         this.displayTodos();
     },
+    // TODO not completed this part
     changeTodo:function(position,value){
         this.todos[position]= value;
         this.displayTodos();
     },
+    // TODO not completed this part
     deleteTodo:function(position){
         this.todos.splice(position,1);
             this.displayTodos();
@@ -34,7 +35,11 @@ var viewtodoList ={
         console.log(todosUl);
         for(var i=0; i < todoList.todos.length; i++){
             var todoLi = document.createElement('li');
+            var radioBox = document.createElement('input');
+            var labelTxt = document.createElement('label');
+            radioBox.type ='radio';
             todoLi.textContent = todoList.todos[i];
+            todoLi.appendChild(radioBox,labelTxt);
             todosUl.appendChild(todoLi);
 
         }
@@ -42,10 +47,9 @@ var viewtodoList ={
     }
 }
 
-document.querySelector('.new-todo').addEventListener('keypress', function (e) {
+document.querySelector('.new-todo').addEventListener('keypress', function (ev) {
     var todoText =  document.getElementById('txt-addtodo').value;
-
-    var key = e.which || e.keyCode;
+    var key = ev.which || ev.keyCode;
     if (key === 13) { // 13 is enter
       todoList.addTodo(todoText);
         viewtodoList.displayTodos();
